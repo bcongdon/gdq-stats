edf9d947a034ef26b4bcc2d8a0651f6305bde20e
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-04 14:03:24 -0700

    More realtime stats

--- stats.js
+++ stats.js
@@ -2,17 +2,31 @@
 var client = irc.client()
 function queryTwitch(){
     client.api({
-        url: "https://api.twitch.tv/kraken/streams/edward50054"
+        url: "https://api.twitch.tv/kraken/streams/wizardworldgaming"
     }, function(err, res, body) {
         if(body && "stream" in body && "viewers" in body['stream']){
-            console.log("Current viewers: " + body['stream']['viewers']);
-            odometer.innerHTML = body['stream']['viewers'];
+            console.debug("Current viewers: " + body['stream']['viewers']);
+            oViewers.innerHTML = body['stream']['viewers'];
         }
     });
 }
 
+function queryDonations(){
+
+    setTimeout(function() {oDonations.innerHTML = "0"}, 1000);
+}
+
+function gamesCompleted(){
+    setTimeout(function() {oGames.innerHTML = "163"}, 1000);
+}
+
 queryTwitch();
+queryDonations();
+gamesCompleted();
 
 setInterval(function(){
     queryTwitch();
+    queryDonations();
+    gamesCompleted();
+
 }, 10000);
