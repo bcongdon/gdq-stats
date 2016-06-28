31641f068d119c1bbe87bd1e7c87e39c740b312e
Benjamin Congdon <bencongdon96@gmail.com>
2016-06-28 09:24:43 -0700

    Grab media link to get latest generation

--- js/graph.d3.js
+++ js/graph.d3.js
@@ -526,27 +526,29 @@ function loadSelectCookies() {
   }
 }
 
-$.ajax({
-  url: 'https://www.googleapis.com/download/storage/v1/b/sgdq-backend.appspot.com/o/latest.json?alt=media',
-  async: true,
-  // retryCount and retryLimit will let you retry a determined number of times
-  retryCount: 0,
-  retryLimit: 5,
-  // retryTimeout limits the total time retrying (in milliseconds)
-  retryTimeout: 15000,
-  // timeout for each request
-  timeout: 2000,
-  // created tells when this request was created
-  created : Date.now(),
-  error : function(xhr, textStatus, errorThrown ) {
-    this.retryCount++;
-    if (this.retryCount <= this.retryLimit && Date.now() - this.created < this.retryTimeout) {
-      console.log("Retrying");
-      $.ajax(this);
-      return;
-    }
-  },
-  success: setupAll
+$.get('https://www.googleapis.com/storage/v1/b/sgdq-backend.appspot.com/o/latest.json', function(response){
+  $.ajax({
+    url: response.mediaLink,
+    async: true,
+    // retryCount and retryLimit will let you retry a determined number of times
+    retryCount: 0,
+    retryLimit: 5,
+    // retryTimeout limits the total time retrying (in milliseconds)
+    retryTimeout: 15000,
+    // timeout for each request
+    timeout: 2000,
+    // created tells when this request was created
+    created : Date.now(),
+    error : function(xhr, textStatus, errorThrown ) {
+      this.retryCount++;
+      if (this.retryCount <= this.retryLimit && Date.now() - this.created < this.retryTimeout) {
+        console.log("Retrying");
+        $.ajax(this);
+        return;
+      }
+    },
+    success: setupAll
+  });
 });
 
 function setupAll(res) {
