0658533b65f2cc0720743574dbcae0f51da7a033
Benjamin Congdon <bencongdon96@gmail.com>
2016-07-02 10:31:50 -0700

    Significant work towards custom game scatter graph

--- js/games_scatter.js
+++ js/games_scatter.js
@@ -4,23 +4,34 @@ $.get('/data/games.csv')
     handleData(rows)
   });
 
+function unique(arr) {
+  return arr.filter(function (e, i, arr) {
+    return arr.lastIndexOf(e) === i;
+  });
+}
+
 function handleData(data) {
   var titles       = data.map(function(d) { return d[1]; }),
       runners      = data.map(function(d) { return d[2]; }),
-      platform     = data.map(function(d) { return d[3]; }),
+      platforms    = data.map(function(d) { return d[3]; }),
       releaseDates = data.map(function(d) { return d[4]; }),
-      minutes      = data.map(function(d) { return d[5]; });
+      minutes      = data.map(function(d) { return d[5]; }),
+      platform_unique = unique(platforms);
   releaseDates.unshift('x');
-  minutes.unshift('minutes')
+  releaseDates = releaseDates.map(function(d) {return d == "" ? null : d;})
+  var columns = [releaseDates];
+  for(var i in platform_unique) {
+    var newCol = data.map(function(d) { return d[3] == platform_unique[i] ? d[5] : null});
+    newCol.unshift(platform_unique[i])
+    columns.push(newCol)
+  }
+  console.log(columns.filter(function(d) { return d[0] == "SNES"}))
   console.log(releaseDates)
   var chart = c3.generate({
       bindto: '#chart',
       data: {
           x: 'x',
-          columns: [
-            releaseDates,
-            minutes
-          ],
+          columns: columns,
           type: 'scatter'
       },
       axis: {
@@ -36,10 +47,41 @@ function handleData(data) {
               label: 'SGDQ Speedrun Time'
           }
       },
+      point: {
+        r: 3
+      },
       tooltip: {
         format: {
-          title: function(d) { return d; },
-          value: function(value, ratio, id) { return 5 }
+          title: function(d) { 
+            var date = moment(d).format('YYYY-MM-DD');
+            return titles[releaseDates.lastIndexOf(date) - 1] + '<br>' + date;
+          },
+          value: function(value, ratio, id) { return value + ' minutes' }
+        },
+        contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+          var $$ = this, config = $$.config,
+              titleFormat = config.tooltip_format_title || defaultTitleFormat,
+              nameFormat = config.tooltip_format_name || function (name) { return name; },
+              valueFormat = config.tooltip_format_value || defaultValueFormat,
+              text, i, title, value, name, bgcolor;
+          for (i = 0; i < d.length; i++) {
+              if (! (d[i] && (d[i].value || d[i].value === 0))) { continue; }
+
+              if (! text) {
+                  title = titleFormat ? titleFormat(d[i].x) : d[i].x;
+                  text = "<table class='" + $$.CLASS.tooltip + "'>" + (title || title === 0 ? "<tr><th colspan='2'>" + title + "</th></tr>" : "");
+              }
+
+              name = nameFormat(d[i].name);
+              value = valueFormat(d[i].value, d[i].ratio, d[i].id, d[i].index);
+              bgcolor = $$.levelColor ? $$.levelColor(d[i].value) : color(d[i].id);
+
+              text += "<tr class='" + $$.CLASS.tooltipName + "-" + d[i].id + "'>";
+              text += "<td class='name'><span style='background-color:" + bgcolor + "'></span>" + name + "</td>";
+              text += "<td class='value'>" + value + "</td>";
+              text += "</tr>";
+          }
+          return text + "</table>";
         }
       }
   });
