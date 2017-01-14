35d1be3b56b6e7cded04823fc12029c07e90d441
Benjamin Congdon <bencongdon96@gmail.com>
2017-01-13 17:55:24 -0800

    Fix killVsSave, add it to main page

--- js/bid_series.js
+++ js/bid_series.js
@@ -1,36 +1,38 @@
-function accumulate(arr) {
-  return arr.reduce(function(r, a) {
-    r.push((r.length && r[r.length - 1] || 0) + a);
-    return r;
-  }, []);  
-}
+'use strict';
 
-// // Grab metadata on the latest json
-// getRetry('https://www.googleapis.com/storage/v1/b/sgdq-backend.appspot.com/o/killVsSave.json', function(res) {
-//   // Get media link and pipe data to setupAll
-//   getRetry(res.mediaLink, setupBidSeries)
-// });
-
-getRetry('/data/2016/killVsSave.json', setupBidSeries);
+getRetry('http://storage.api.gdqstat.us/killVsSave.json', function(res) {
+  setupBidSeries(res)
+});
 
 function setupBidSeries(data) {
-  // data = JSON.parse(data)
-  var accumulatedYs = data.ys.map(function(d) { return accumulate(d.slice(1)) })
-  accumulatedYs.forEach(function(d, idx) { d.unshift(data.ys[idx][0]) })
-  var chart = graphSeries('#metroid_chat', data.x, accumulatedYs);
-  chart.data.colors({
-    'Save': d3.rgb('#00AEEF'),
-    'Kill': d3.rgb('#F21847')
-  });
+  data = JSON.parse(data)
+  var xVals = data.map(function(d) { return new Date(d.time) });
+  xVals.unshift('x')
+  var killVals = ['Kill'],
+      saveVals = ['Save'];
+  for (var i = 0; i < data.length; i++) {
+    if(i > 0) {
+      data[i].save += data[i - 1].save;
+      data[i].kill += data[i - 1].kill;
+    }
+    killVals.push(data[i].kill);
+    saveVals.push(data[i].save);
+  }
+  graphSeries('#metroid_chart', xVals, [killVals, saveVals]);
 }
 
 function graphSeries(element, xSeries, yArrs){
   var cols = [xSeries].concat(yArrs);
-  return chart = c3.generate({
+  console.log(cols)
+  return c3.generate({
     bindto: element,
     data: {
       x: 'x',
-      columns: cols
+      columns: cols,
+      colors: {
+        'Save': d3.rgb('#00AEEF'),
+        'Kill': d3.rgb('#F21847')
+      }
     },
     axis: {
       x: {
