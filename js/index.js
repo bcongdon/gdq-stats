282f98045c38734472b10614ebd007e4827b854c
Benjamin Congdon <bencongdon96@gmail.com>
2017-06-30 08:17:21 -0700

    Graph parametrized by query args

--- js/index.js
+++ js/index.js
@@ -6,9 +6,37 @@ import { Provider } from 'react-redux'
 import reducers from './reducers'
 import reduxThunk from 'redux-thunk'
 import 'react-select/dist/react-select.css'
+import { setCurrentSeries, setButtonZoom } from './actions'
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
+
+store.subscribe(() => {
+  const state = store.getState()
+  const urlParams = new URLSearchParams(window.location.search)
+  urlParams.set('series', state.gdq.series)
+  if(state.gdq.activeButtonZoomIndex >= 0){
+    urlParams.set('zoom', state.gdq.activeButtonZoomIndex)
+  } else {
+    urlParams.delete('zoom')
+  }
+  // if(state.gdq.activeGameZoom) {
+  //   urlParams.set('game', state.gdq.activeGameZoom.name)
+  // }
+  const baseUrl = [location.protocol, '//', location.host, location.pathname].join('')
+  history.replaceState('', '', '?' + urlParams.toString())
+})
+
+const urlParams = new URLSearchParams(window.location.search)
+if(urlParams.has('series')) {
+  setCurrentSeries(parseInt(urlParams.get('series')))(store.dispatch)
+}
+if(urlParams.has('zoom')) {
+  setButtonZoom(parseInt(urlParams.get('zoom')))(store.dispatch)
+}
+if(urlParams.has('game')) {
+
+}
 
 ReactDOM.render(
   <Provider store={store}>
