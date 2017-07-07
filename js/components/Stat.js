c0461356b085b462504681a9febbcc93a98509f9
bcongdon <bencongdon96@gmail.com>
2017-07-07 15:11:13 -0700

    Fix odometer spacing on smaller screen sizes

--- js/components/Stat.js
+++ js/components/Stat.js
@@ -21,10 +21,12 @@ export default class Stat extends React.PureComponent {
       <Col lg={4} md={6} xs={12} className='stat'>
         <span className='current_header'>{this.props.title}:</span>
         <div className='odometer-group'>
-          {this.props.prefix}
-          {this.props.value === 0
-            ? <span style={{paddingRight: 5}}>0</span>
-            : <Odometer value={this.props.value} options={{format: this.props.format}} />}
+          <div style={{minWidth: 60, textAlign: 'right'}}>
+            {this.props.prefix}
+            {this.props.value === 0
+              ? <span style={{paddingRight: 5}}>0</span>
+              : (<Odometer value={this.props.value} options={{format: this.props.format}} />)}
+          </div>
           <div className='emoji'>{this.props.emoji}</div>
         </div>
       </Col>
