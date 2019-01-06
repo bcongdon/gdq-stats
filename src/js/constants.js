0f582ce3bbb7b8ce8b0b2f6fe59d1e13668305e4
Benjamin Congdon <bencongdon96@gmail.com>
2019-01-06 08:55:45 -0800

    Deploy for AGDQ 2019

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,14 +3,14 @@ import dayjs from 'dayjs'
 
 export const GDQ_API_ENDPOINT = 'https://api.gdqstat.us'
 
-export const OFFLINE_MODE = true
+export const OFFLINE_MODE = false
 
 const LIVE_STORAGE_ENDPOINT = 'https://storage.api.gdqstat.us'
 
 // Note: Keep this up-to-date with the most recent event
-export const EVENT_YEAR = 2018
-export const EVENT_SHORT_NAME = 'sgdq'
-export const EVENT_START_DATE = dayjs('6-24-18')
+export const EVENT_YEAR = 2019
+export const EVENT_SHORT_NAME = 'agdq'
+export const EVENT_START_DATE = dayjs('01-06-19')
 
 const OFFLINE_STORAGE_ENDPOINT = `/data/${EVENT_YEAR}/${EVENT_SHORT_NAME}_final`
 
