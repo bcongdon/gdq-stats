add5645540fd1643566fd8aba96450b22026cd76
Benjamin Congdon <bencongdon96@gmail.com>
2021-01-03 07:58:43 -0800

    AGDQ 2021 changes

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,14 +3,14 @@ import dayjs from "dayjs";
 
 export const GDQ_API_ENDPOINT = "https://api.gdqstat.us";
 
-export const OFFLINE_MODE = true;
+export const OFFLINE_MODE = false;
 
 const LIVE_STORAGE_ENDPOINT = "https://storage.api.gdqstat.us";
 
 // Note: Keep this up-to-date with the most recent event
 export const EVENT_YEAR = 2020;
-export const EVENT_SHORT_NAME = "sgdq";
-export const EVENT_START_DATE = dayjs("08-16-20");
+export const EVENT_SHORT_NAME = "agdq";
+export const EVENT_START_DATE = dayjs("01-03-21");
 
 const OFFLINE_STORAGE_ENDPOINT = `/data/${EVENT_YEAR}/${EVENT_SHORT_NAME}_final`;
 
